f81db6f3a2dc86decb0923883a5b45b1beb71d70
Shubhrajit1pallob <spallob@asu.edu>
2025-07-10 19:35:52 -0700

    Made changes to the provider.tf and instance.tf files.

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -18,22 +18,22 @@ data "aws_ami" "ubuntu" {
 
 # This Terraform configuration deploys an AWS EC2 instance with a specified AMI, security group, IAM role, and SSH key pair.
 resource "aws_instance" "this" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.deployer_key.key_name
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.developer.key_name
   vpc_security_group_ids = [aws_security_group.this.id]
-  iam_instance_profile = aws_iam_instance_profile.this.name
+  iam_instance_profile   = aws_iam_instance_profile.this.name
 
   root_block_device {
-    volume_size = 10
-    volume_type = "gp2"
+    volume_size           = 10
+    volume_type           = "gp2"
     delete_on_termination = true
   }
 
   connection {
-    type = "ssh"
-    user = "ubuntu"
-    host = self.public_ip
+    type        = "ssh"
+    user        = "ubuntu"
+    host        = self.public_ip
     private_key = var.private_key
   }
 
@@ -43,8 +43,8 @@ resource "aws_instance" "this" {
 }
 
 # This configuration creates an SSH key pair for accessing the EC2 instance.
-resource "aws_key_pair" "deployer" {
-  key_name = var.key_name
+resource "aws_key_pair" "developer" {
+  key_name   = var.key_name
   public_key = var.public_key
 }
 
@@ -52,37 +52,41 @@ resource "aws_key_pair" "deployer" {
 resource "aws_security_group" "this" {
   egress = [
     {
-        cidr_blocks = ["0.0.0.0/0"]
-        description = "Allow all outbound traffic"
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        security_groups = []
-        ipv6_cidr_blocks = []
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "Allow all outbound traffic"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      security_groups  = []
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
     }
   ]
 
   ingress = [
     {
-        cidr_blocks = ["0.0.0.0/0"]
-        description = "Allow all inbound traffic"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        security_groups = []
-        ipv6_cidr_blocks = []
-        self = false
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "Allow all inbound traffic"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      security_groups  = []
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
     },
 
     {
-        cidr_blocks = ["0.0.0.0/0"]
-        description = "Allow all inbound traffic"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        security_groups = []
-        ipv6_cidr_blocks = []
-        self = false
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "Allow all inbound traffic"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      security_groups  = []
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
     }
   ]
 }
--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
   backend "s3" {
     bucket = "full-scale-app-automation-cicd"
-    key = "aws/ec2-depoy/terraform.tfstate"
+    key    = "aws/ec2-depoy/terraform.tfstate"
     region = "us-east-1"
   }
   required_version = "~> 1.0"
